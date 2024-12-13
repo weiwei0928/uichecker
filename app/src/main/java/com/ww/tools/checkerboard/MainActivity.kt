fd59a4d0e9830114e9216e8fd82540c52a6141fd
weiwei <weiwei@zhizhangyi.com>
2024-12-13 10:18:12 +0800

    添加readme  测试代码 提交github

--- app/src/main/java/com/ww/tools/checkerboard/MainActivity.kt
+++ app/src/main/java/com/ww/tools/checkerboard/MainActivity.kt
@@ -2,8 +2,6 @@ package com.ww.tools.checkerboard
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import com.ww.tools.uichecker.UIChecker
-import com.ww.tools.checkerboard.R
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,6 +9,5 @@ class MainActivity : ComponentActivity() {
 //        enableEdgeToEdge()
 
         setContentView(R.layout.activity_main)
-        UIChecker.manualInstall(this.application)
     }
 }
