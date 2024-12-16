08458ee7b6d82e7a26a943ab83609b3c0493b8e6
weiwei <weiwei@zhizhangyi.com>
2024-12-16 21:29:56 +0800

    修改jdk版本  修改AGP版本  修改gitignore

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -7,6 +7,7 @@ pluginManagement {
                 includeGroupByRegex("androidx.*")
             }
         }
+        maven(url = uri("./repo"))
         mavenCentral()
         gradlePluginPortal()
     }
@@ -14,6 +15,7 @@ pluginManagement {
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
+        maven(url = uri("./repo"))
         google()
         mavenCentral()
     }
